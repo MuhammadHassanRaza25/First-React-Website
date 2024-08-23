f5d5d659d3da508d6bebc277c87cc82434fe490d
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2024-08-24 03:05:33 +0500

    First React Website

---
+++ src/App.jsx
@@ -0,0 +1,360 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* Header Start*/}
+    <header className="text-gray-600 body-font">
+  <div className="container-fluid mx-auto flex flex-wrap p-7 flex-col md:flex-row items-center">
+    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <span className="ml-3 text-3xl flex"><img className="w-14" src="https://cdn.dribbble.com/users/2442115/screenshots/8699490/dec-01-2019_19-16-16.gif" alt="image" /> React Practice</span>
+    </a>
+    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <a className="mr-5 hover:text-gray-500 font-bold">Home</a>
+      <a className="mr-5 hover:text-gray-500 font-bold">About</a>
+      <a className="mr-5 hover:text-gray-500 font-bold">Contact Us</a>
+    </nav>
+    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+    <a href="https://github.com/MuhammadHassanRaza25" target="_blank">Github</a>
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        className="w-4 h-4 ml-1"
+        viewBox="0 0 24 24"
+      >
+        <path d="M5 12h14M12 5l7 7-7 7" />
+      </svg>
+    </button>
+  </div>
+</header>
+{/* Header End */}
+
+{/* Hero Section Start */}
+<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+       Hi,
+        <br className="hidden lg:inline-block" />
+        I'm Muhammad Hassan Raza <br />
+        <span className='flex'>Front End Developer <img className='w-8 h-8 mt-2.5 ml-2' src="https://media.tenor.com/QhRvvwpCdVoAAAAj/rocket.gif" alt="" /></span>
+      </h1>
+      <p className="mb-8 leading-relaxed">
+      I’m Muhammad Hassan Raza, a Front End Developer dedicated to crafting engaging web experiences. I created this simple React website as a practice project to refine my skills and showcase my understanding of React concepts. Explore my work to see my approach to building clean and functional user interfaces.
+      </p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          <a href="https://github.com/MuhammadHassanRaza25" target="_blank">Github</a>
+        </button>
+        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+           <a href="https://www.linkedin.com/in/muhammad-hassan-raza-aab5402b7/" target="_blank">Linkedin</a>
+        </button>
+      </div>
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img
+        className="object-cover object-center w-full rounded-xl shadow-black shadow-lg"
+        alt="hero"
+        src="https://images.unsplash.com/photo-1672309046475-4cce2039f342?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJlYWN0JTIwanN8ZW58MHx8MHx8fDA%3D"
+      />
+    </div>
+  </div>
+</section>
+{/* Hero Section End */}
+
+{/* Cards Section Start */}
+<section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img
+            className="lg:h-48 md:h-36 w-full object-cover object-center"
+            src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHRlY2h8ZW58MHx8MHx8fDA%3D"
+            alt="blog"
+          />
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+              CATEGORY
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              Web Developer
+            </h1>
+            <p className="leading-relaxed mb-3">
+            Web developers are digital architects who build and maintain the online spaces where we live, work, and play.
+            </p>
+            <div className="flex items-center flex-wrap ">
+              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </a>
+              <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                <svg
+                  className="w-4 h-4 mr-1"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                  <circle cx={12} cy={12} r={3} />
+                </svg>
+                1.5K
+              </span>
+              <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                <svg
+                  className="w-4 h-4 mr-1"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                </svg>
+                1
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img
+            className="lg:h-48 md:h-36 w-full object-cover object-center"
+            src="https://plus.unsplash.com/premium_photo-1683121710572-7723bd2e235d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE3fHx0ZWNobm9sb2d5fGVufDB8fDB8fHww"
+            alt="blog"
+          />
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+              CATEGORY
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              Technology
+            </h1>
+            <p className="leading-relaxed mb-3">
+            Technology is the catalyst that transforms ideas into innovations, shaping the future one advancement at a time.
+            </p>
+            <div className="flex items-center flex-wrap">
+              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </a>
+              <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                <svg
+                  className="w-4 h-4 mr-1"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                  <circle cx={12} cy={12} r={3} />
+                </svg>
+                1.1K
+              </span>
+              <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                <svg
+                  className="w-4 h-4 mr-1"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                </svg>
+                3
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img
+            className="lg:h-48 md:h-36 w-full object-cover object-center"
+            src="https://images.unsplash.com/photo-1605648916361-9bc12ad6a569?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE2fHx0ZWNobm9sb2d5fGVufDB8fDB8fHww"
+            alt="blog"
+          />
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+              CATEGORY
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              Speaker
+            </h1>
+            <p className="leading-relaxed mb-3">
+              A Bluetooth speaker delivers wireless freedom, letting you enjoy high-quality sound from your favorite devices without the clutter of cords.
+            </p>
+            <div className="flex items-center flex-wrap ">
+              <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                Learn More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </a>
+              <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                <svg
+                  className="w-4 h-4 mr-1"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                  <circle cx={12} cy={12} r={3} />
+                </svg>
+                4K
+              </span>
+              <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                <svg
+                  className="w-4 h-4 mr-1"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                </svg>
+                9
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+{/* Cards Section Start */}
+
+{/* Footer Start */}
+<footer className="text-gray-600 body-font">
+  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      <span className="ml-3 text-xl flex items-center"><img className="w-14" src="https://cdn.dribbble.com/users/2442115/screenshots/8699490/dec-01-2019_19-16-16.gif" alt="image" /><span className='mb-1'>React Practice</span> </span>
+    </a>
+    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+      © 2024 —
+      <a
+        href="https://twitter.com/knyttneve"
+        className="text-gray-600 ml-1"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Muhammad Hassan Raza
+      </a>
+    </p>
+    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      <a className="text-gray-500">
+        <svg
+          fill="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          className="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+        </svg>
+      </a>
+      <a className="ml-3 text-gray-500">
+        <svg
+          fill="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          className="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+        </svg>
+      </a>
+      <a className="ml-3 text-gray-500">
+        <svg
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          className="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+        </svg>
+      </a>
+      <a className="ml-3 text-gray-500">
+        <svg
+          fill="currentColor"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={0}
+          className="w-5 h-5"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="none"
+            d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+          />
+          <circle cx={4} cy={4} r={2} stroke="none" />
+        </svg>
+      </a>
+    </span>
+  </div>
+</footer>
+{/* Footer End */}
+    </>
+  )
+}
+
+export default App
